70a5bca9dcb615512c6eef5260d13afaf35836a0
Mariano D'Agostino <mariano-dagostino@users.noreply.github.com>
2016-07-04 10:27:16 -0300

    Make tests easier to read

--- test_AcmeTinyHelper.py
+++ test_AcmeTinyHelper.py
@@ -2,24 +2,36 @@ from AcmeTinyHelper import SSLManager
 
 class TestAcmeTinyHelper:
 
+
+
     def setup_method(self, method):
         self.manager = SSLManager('/my/path', ['example.com'])
         self.manager.returnString = True
 
+
+
     def test_makePath(self):
         assert self.manager._makePath() == 'mkdir -p /my/path'
+
+
 
     def test_callOpenSSL(self):
         assert self.manager._callOpenSSL('account.key') == 'openssl genrsa 4096 > /my/path/account.key'
         assert self.manager._callOpenSSL('domain.key')  == 'openssl genrsa 4096 > /my/path/domain.key'
 
+
+
     def test_DiffieHellmanParams(self):
         expected = 'openssl dhparam -out /my/path/dhparam.pem 4096'
         assert self.manager._getDiffieHellmanParams() == expected
 
+
+
     def test_getIntermediateCertificate(self):
         expected = 'wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > /my/path/intermediate.pem'
         assert self.manager._getIntermediateCertificate() == expected
+
+
 
     def test_getSelfSignedCertificate(self):
         expected = 'openssl req -new -sha256 -key /my/path/domain.key -subj "/CN=example.com" > /my/path/domain.csr'
@@ -29,6 +41,8 @@ class TestAcmeTinyHelper:
         expected = 'openssl req -new -sha256 -key /my/path/domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:www.example.com,DNS:example.com,DNS:subdomain.example.com")) > domain.csr'
         assert self.manager._getSelfSignedCertificate() == expected
 
+
+
     def test_getSignedCertificate(self):
         assert self.manager._getSignedCertificate() == 'python /bin/acme_tiny.py --account-key /my/path/account.key --csr /my/path/domain.csr --acme-dir /var/www/challenges > /my/path/signed.crt'
 
@@ -37,6 +51,8 @@ class TestAcmeTinyHelper:
 
         self.manager.challenge_path = '/tmp/challenges'
         assert self.manager._getSignedCertificate() == 'python /usr/bin/acme_tiny.py --account-key /my/path/account.key --csr /my/path/domain.csr --acme-dir /tmp/challenges > /my/path/signed.crt'
+
+
 
     def test_chainCertificates(self):
         expected = 'cat /my/path/signed.crt /my/path/intermediate.pem > /my/path/chained.pem'
